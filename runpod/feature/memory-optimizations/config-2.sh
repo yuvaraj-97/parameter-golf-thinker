f9f9c5f715a945842080484fb41e13efe6e7c1f2
Yuvaraj A <mailtoyuvaraj11@gmail.com>
2026-04-10 16:07:11 +0530

    Add RunPod go-go launcher for memory configs

---
+++ runpod/feature/memory-optimizations/config-2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+set -eu
+
+RUN_ID="${RUN_ID:-ckpt_higher_batch}" \
+DATA_PATH="${DATA_PATH:-./data/datasets/fineweb10B_sp1024/}" \
+TOKENIZER_PATH="${TOKENIZER_PATH:-./data/tokenizers/fineweb_1024_bpe.model}" \
+VOCAB_SIZE="${VOCAB_SIZE:-1024}" \
+USE_CHECKPOINTING="${USE_CHECKPOINTING:-1}" \
+TRAIN_BATCH_TOKENS="${TRAIN_BATCH_TOKENS:-655360}" \
+VAL_BATCH_SIZE="${VAL_BATCH_SIZE:-262144}" \
+torchrun --standalone --nproc_per_node="${NPROC_PER_NODE:-8}" train_gpt.py
